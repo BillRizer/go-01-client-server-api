7531b742a6d22e0718011bde695229ea6b5a2c11
Telis <pablo.goncalves@eduzz.com.br>
2025-03-21 18:44:28 -0500

    Update README and implement client-server functionality for USD to BRL exchange rates

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	logger "challengerone/logger"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -9,36 +10,22 @@ import (
 	"time"
 )
 
-type Response struct {
-	Usdbrl struct {
-		Code       string `json:"code"`
-		Codein     string `json:"codein"`
-		Name       string `json:"name"`
-		High       string `json:"high"`
-		Low        string `json:"low"`
-		VarBid     string `json:"varBid"`
-		PctChange  string `json:"pctChange"`
-		Bid        string `json:"bid"`
-		Ask        string `json:"ask"`
-		Timestamp  string `json:"timestamp"`
-		CreateDate string `json:"create_date"`
-	} `json:"USDBRL"`
+type BidResponse struct {
+	Bid string `json:"bid"`
 }
 
-func fetch(ctx context.Context) (*Response, error) {
-	const url = "http://localhost:3333/cotation"
+func fetch(ctx context.Context) (*BidResponse, error) {
+	const url = "http://localhost:8080/cotation"
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Error when creating request: %w", err)
 	}
-
-	client := &http.Client{}
-	response, err := client.Do(req)
+	response, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("Error when making request: %w", err)
 	}
 	defer response.Body.Close()
-	var data Response
+	var data BidResponse
 	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
 		return nil, fmt.Errorf("Error parsing JSON: %w", err)
 	}
@@ -59,21 +46,20 @@ func saveToFile(filename, data string) error {
 }
 
 func main() {
+	logger.InitLogger("client.log")
 	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, time.Millisecond)
+	ctx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
 	defer cancel()
 	response, err := fetch(ctx)
 	if err != nil {
 		fmt.Printf("Error fetching data: %v\n", err)
 		return
 	}
-	text := fmt.Sprintf("Dólar: %s", response.Usdbrl.Bid)
+	text := fmt.Sprintf("Dólar: %s", response.Bid)
 	errSaveFile := saveToFile("cotacao.txt", text)
 	if errSaveFile != nil {
 		fmt.Println("Erro ao salvar o arquivo:", err)
 		return
 	}
-	fmt.Print(response)
-	fmt.Println("Hello, World!")
-	fmt.Println("This is a Go program")
+	fmt.Printf("Dólar: %s", response.Bid)
 }
--- server.go
+++ server.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
+
+	logger "challengerone/logger"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Response struct {
@@ -23,10 +31,44 @@ type Response struct {
 		CreateDate string `json:"create_date"`
 	} `json:"USDBRL"`
 }
+type BidResponse struct {
+	Bid string `json:"bid"`
+}
 
-func fetch() (*Response, error) {
+func saveCotation(ctx context.Context, data Response) error {
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		return fmt.Errorf("erro ao conectar ao banco de dados: %v", err)
+	}
+	defer db.Close()
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS cotations (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		code TEXT,
+		codein TEXT,
+		bid TEXT,
+		ask TEXT,
+		create_date TEXT
+	);`
+	_, err = db.ExecContext(ctx, createTableSQL)
+	if err != nil {
+		return fmt.Errorf("erro ao criar a tabela: %v", err)
+	}
+	insertSQL := `INSERT INTO cotations (code,codein,bid,ask,create_date) VALUES (?, ?, ?, ?, ?)`
+	_, err = db.ExecContext(ctx, insertSQL, data.Usdbrl.Code, data.Usdbrl.Codein, data.Usdbrl.Bid, data.Usdbrl.Ask, data.Usdbrl.CreateDate)
+	if err != nil {
+		return fmt.Errorf("erro ao inserir os dados: %v", err)
+	}
+	return nil
+}
+
+func fetch(ctx context.Context) (*Response, error) {
 	const url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-	response, err := http.Get(url)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating request: %w", err)
+	}
+	response, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("Error fetching data: %w", err)
 	}
@@ -39,25 +81,38 @@ func fetch() (*Response, error) {
 }
 
 func getCotation(w http.ResponseWriter, r *http.Request) {
-	response, err := fetch()
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+	response, err := fetch(ctx)
 	if err != nil {
-		fmt.Printf("Error fetching data: %v\n", err)
+		log.Printf("Erro ao buscar a cotação: %v\n", err)
+		return
+	}
+	ctxDb, cancelDb := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancelDb()
+	errSave := saveCotation(ctxDb, *response)
+	if errSave != nil {
+		log.Printf("Erro ao salvar a cotação: %v\n", err)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(BidResponse{
+		Bid: response.Usdbrl.Bid,
+	})
 }
 
 func main() {
-	fmt.Println("Starting server...")
+	logger.InitLogger("server.log")
+
+	log.Printf("Starting server...")
 	http.HandleFunc("/cotation", getCotation)
 
 	err := http.ListenAndServe(":8080", nil)
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
+		log.Printf("server closed\n")
 	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
+		log.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
 }
